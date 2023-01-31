5634770593186b6f10c69a009b470f4249ca9a49
Lucky <105638238+luck1004@users.noreply.github.com>
2023-01-31 23:51:39 +0900

    Add files via upload

---
+++ genie.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('DB URL')
+db = client.dbsparta
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# rank
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.number
+
+# title
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.info > a.title.ellipsis
+
+# artist
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.info > a.artist.ellipsis
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for tr in trs:
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text
+    if "19금" in title:
+        title = title.replace("\n", "")
+        title = title.replace(" ", "")
+    
+    doc = {
+        'rank':rank,
+        'title':title,
+        'artist':artist
+    }
+    db.genieMusic.insert_one(doc)
